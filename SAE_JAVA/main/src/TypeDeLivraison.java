681ba5a00ddb2285c78d20f5319d47e35c49812c
Valpwz <val.pwz@gmail.com>
2025-05-16 12:18:09 +0200

    Ajout de la méthode choisirLivraison() et modification de la classe TypeDeLivraison

--- SAE_JAVA/main/src/TypeDeLivraison.java
+++ SAE_JAVA/main/src/TypeDeLivraison.java
@@ -5,20 +5,38 @@
  */
 public class TypeDeLivraison {
 
-  //
-  // Fields
-  //
+  private int idlivraison;
+  private String type;
 
   
   //
   // Constructors
   //
-  public TypeDeLivraison () { };
+  public TypeDeLivraison (int idlivraison,
+   String type) {
+    this.idlivraison = idlivraison;
+    this.type = type;
+    };
   
   //
   // Methods
   //
+  public int getIdlivraison() {
+    return idlivraison;
+  }
+  public void setIdlivraison(int idlivraison) {
+      this.idlivraison = idlivraison;
+  }
+  public String getType() {
+      return type;
+  }
+  public void setType(String type) {
+      this.type = type;
+  }
 
+  public enum TypeLivraison {
+    MAGASIN, DOMICILE;
+  }
 
   //
   // Accessor methods
