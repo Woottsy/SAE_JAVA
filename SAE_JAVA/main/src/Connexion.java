fc9fa42aaf5cb5fb97ce8cf2d1602c0bb94bfea5
Woottsy <wyatt.lacroix@etu.univ-orleans.fr>
2025-05-28 20:42:25 +0200

    Modification majeur de Connexion.java
    à verifier
    à modifier pour utiliser à l'université : ligne 13

--- SAE_JAVA/main/src/Connexion.java
+++ SAE_JAVA/main/src/Connexion.java
@@ -1,20 +1,37 @@
-public class Connexion{
-	private String login;
-	private String mdp;
-	public Connexion(String login, String mdp) {
-		this.login = login;
-		this.mdp = mdp;
-	}
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getMdp() {
-		return mdp;
-	}
-	public void setMdp(String mdp) {
-		this.mdp = mdp;
-	}
+import java.sql.*;
+
+public class Connexion {
+    private Connection connexion = null;
+    private boolean connecte = false;
+
+    public Connexion() throws ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+    }
+
+    public void connecter(String nomServeur, String nomBase, String nomLogin, String motDePasse) throws SQLException {
+        this.connexion = DriverManager.getConnection(
+            "jdbc:mysql://" + nomServeur + ":3306/" + nomBase,
+            nomLogin, motDePasse
+        );
+        this.connecte = this.connexion != null;
+    }
+
+    public void close() throws SQLException {
+        if (this.connexion != null && !this.connexion.isClosed()) {
+            this.connexion.close();
+            this.connecte = false;
+        }
+    }
+
+    public boolean isConnecte() {
+        return this.connecte;
+    }
+
+    public Statement createStatement() throws SQLException {
+        return this.connexion.createStatement();
+    }
+
+    public PreparedStatement prepareStatement(String requete) throws SQLException {
+        return this.connexion.prepareStatement(requete);
+    }
 }
