1cb6aeec026eba5857ad7da4dd3f093ff1bdd6a5
Woottsy <wyatt.lacroix@etu.univ-orleans.fr>
2025-05-28 20:33:51 +0200

    création de la classe Connexion.java
    à tester
    
    à modifier : ligne 17.

---
+++ SAE_JAVA/main/src/Connexion.java
@@ -0,0 +1,20 @@
+public class Connexion{
+	private String login;
+	private String mdp;
+	public Connexion(String login, String mdp) {
+		this.login = login;
+		this.mdp = mdp;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getMdp() {
+		return mdp;
+	}
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+}
