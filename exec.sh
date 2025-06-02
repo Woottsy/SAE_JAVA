9ff57a056d8091c168367d4588b5769b8f423624
Lacroix Wyatt <o22401551@or-iut-i003-f06.iut45.univ-orleans.fr>
2025-06-02 12:25:16 +0200

    Modif majeur de AppLibrairie, a compléter

---
+++ exec.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clear
+javac --source-path ./src -d ./bin --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls SAE_JAVA/main/src/*.java
+java -cp ./bin:/usr/share/java/mariadb-java-client.jar --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls AppLibrairie
