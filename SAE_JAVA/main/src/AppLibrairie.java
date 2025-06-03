a0f3d512b9c0f203f5f3e60c9f03c82670ca49f6
Guerrier Titouan <o22401366@or-iut-i003-f31.iut45.univ-orleans.fr>
2025-06-03 08:54:53 +0200

    ajout de l'application sans interface et de la librairie

--- SAE_JAVA/main/src/AppLibrairie.java
+++ SAE_JAVA/main/src/AppLibrairie.java
@@ -9,17 +9,118 @@ public class AppLibrairie {
 	boolean commande_faite = false;
 	while(!commande_faite) {
 	    System.out.println("Que voulez vous faire?");
+	    System.out.println("a: pour vous connecter en tant qu'administrateur");
+	    System.out.println("c: pour vous connecter en tant que client");
+	    System.out.println("v: pour vous connecter en tant que vendeur");
 	    System.out.println("Q: quitter");
 	    String commande_brute = System.console().readLine();
 	    String commande = commande_brute.strip().toLowerCase();
 	    if(commande.equals("q")) {
 		quitter = true;
 		commande_faite = true;
-	    } else {
+	    } 
+		if(commande.equals("a")) {
+		menuAdmin();
+		commande_faite = true;
+	    }
+		if(commande.equals("c")) {
+		menuAcheteur();
+		commande_faite = true;
+	    }
+		if(commande.equals("v")) {
+		menuVendeur();
+		commande_faite = true;
+	    }else {
 		System.out.println("Commande '" + commande_brute + "' invalide.");
 	    }
     }
     }
+	public void menuAdmin() {
+	boolean commande_faite = false;
+	while(!commande_faite) {
+	    System.out.println("Que voulez vous faire?");
+	    System.out.println("g: gérer les rôles");
+	    System.out.println("il: insérer un nouveau livre");
+	    System.out.println("ir: insérer un nouveau role");
+	    System.out.println("s: obtenir les infos souhaiter");
+	    System.out.println("Q: quitter");
+	    String commande_brute = System.console().readLine();
+	    String commande = commande_brute.strip().toLowerCase();
+	    if(commande.equals("q")) {
+		quitter = true;
+		commande_faite = true;
+	    }
+		if(commande.equals("g")) {
+		commande_faite = true;
+		
+	    }
+		if(commande.equals("il")) {
+		commande_faite = true;
+
+	    }
+		if(commande.equals("ir")) {
+		commande_faite = true;
+		
+	    }
+		if(commande.equals("s")) {
+		commande_faite = true;
+		
+	    } 
+		else {
+		System.out.println("Commande '" + commande_brute + "' invalide.");
+	    }
+    }
+    }
+	public void menuVendeur() {
+	boolean commande_faite = false;
+	while(!commande_faite) {
+	    System.out.println("Que voulez vous faire?");
+	    System.out.println("il: insérer un nouveau livre");
+	    System.out.println("m: modifier les infos ");
+	    System.out.println("s: obtenir les infos souhaiter");
+	    System.out.println("Q: quitter");
+	    String commande_brute = System.console().readLine();
+	    String commande = commande_brute.strip().toLowerCase();
+	    if(commande.equals("q")) {
+		quitter = true;
+		commande_faite = true;
+	    } 
+		if(commande.equals("il")) {
+		commande_faite = true;
+
+	    }
+		if(commande.equals("m")) {
+		commande_faite = true;
+		
+	    }
+		if(commande.equals("s")) {
+		commande_faite = true;
+		
+	    } else {
+		System.out.println("Commande '" + commande_brute + "' invalide.");
+	    }
+	}
+	}
+	public void menuAcheteur() {
+	boolean commande_faite = false;
+	while(!commande_faite) {
+	    System.out.println("Que voulez vous faire?");
+	    System.out.println("s: obtenir les infos souhaiter");
+	    System.out.println("Q: quitter");
+	    String commande_brute = System.console().readLine();
+	    String commande = commande_brute.strip().toLowerCase();
+	    if(commande.equals("q")) {
+		quitter = true;
+		commande_faite = true;
+	    } 
+		if(commande.equals("s")) {
+		commande_faite = true;
+		
+	    }else {
+		System.out.println("Commande '" + commande_brute + "' invalide.");
+	    }
+	}
+	}
     public void run() {
 	bienvenue();
 	boolean continuer = true;
@@ -28,14 +129,16 @@ public class AppLibrairie {
 	}
 	au_revoir();
     }
+
+    
     public void bienvenue() {
 	System.out.println("╭────────────────────────────────────────────────────────────────────────────────────╮");
-	System.out.println("│ Bienvenue!Sur cette application tu pourra voir tous les livres dont ut peux réver. │");
+	System.out.println("│ Bienvenue!Sur cette application tu pourra voir tous les livres dont tu peux réver. │");
 	System.out.println("╰────────────────────────────────────────────────────────────────────────────────────╯");
     }
     public void au_revoir() {
 	System.out.println("╭────────────╮");
-	System.out.println("│ Au revoir  │");
+	System.out.println("│ Au revoir .│");
 	System.out.println("╰────────────╯");
     }
 }
