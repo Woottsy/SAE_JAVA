5200b36a2af29f95651b472c4bcadade65e816f7
Guerrier Titouan <o22401366@or-iut-i007-f26.iut45.univ-orleans.fr>
2025-06-10 11:47:52 +0200

    connexion du vendeur

---
+++ SAE_JAVA/main/src/VendeurBD.java
@@ -0,0 +1,50 @@
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class VendeurBD {
+
+    ConnexionMySQL laConnexion;
+    Statement st;
+
+    public VendeurBD(ConnexionMySQL co) {
+        this.laConnexion = co;
+    }
+
+    public int maxnumVendeur() throws SQLException{
+		int maxNum=0;
+		this.st=this.laConnexion.createStatement();
+		ResultSet resultat=st.executeQuery("select ifnull(max(keyVendeur), 0) maxn from VENDEUR");
+		if(resultat.next()){
+
+			maxNum=resultat.getInt("maxn");
+		}
+		return maxNum + 1 ;
+	}
+		    public boolean seConnecter() {
+        try {
+            // Initialisation et récupération de l'id et du mdp
+            Statement st = this.laConnexion.createStatement();
+            System.out.println("Entrez votre identifiant");
+            String id = System.console().readLine();
+            System.out.println("Entrez votre mot de passe");
+            String mdp = System.console().readLine();
+            //Pour afficher le magasin supprimée
+            ResultSet admins = st.executeQuery("select * from VENDEUR");
+            if (admins.next()) {
+                if (id.equals(admins.getString("idVendeur"))) {
+                    if (mdp.equals(admins.getString("motdepasseVendeur"))) {
+                        return true;
+                    }
+                } else {
+                    throw new SQLException();
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Votre Identifiant ou mot de passe est incorrecte");
+        }
+        return false;
+    }
+
+}
