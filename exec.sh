2d75af9a2b54b6759e50107d38e1a9ffed546bcf
Woottsy <wyatt.lacroix@campus.univ-orleans.fr>
2025-06-15 21:02:46 +0000

    Pour la javadoc

--- exec.sh
+++ exec.sh
@@ -7,6 +7,13 @@ javac --source-path SAE_JAVA/main/src -d bin \
   --add-modules javafx.controls \
   SAE_JAVA/main/src/*.java
 
+# Génération de la javadoc
+javadoc --source-path SAE_JAVA/main/src -d doc \
+  --module-path /usr/share/openjfx/lib/ \
+  --add-modules javafx.controls \
+  SAE_JAVA/main/src/*.java
+
+clear
 
 # Exécution (en supposant que AppLibrairie est dans le package sae)
 java -cp bin:/usr/share/java/mariadb-java-client.jar \
